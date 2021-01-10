72d2757f7095a7284643d1c054f925bb0d68ad9e
nowakkamil <nowakkamil@yahoo.com>
2021-01-10 23:55:07 +0100

    Incorporate masking of the image - background removal in the web-server

--- WebServer/web_server.py
+++ WebServer/web_server.py
@@ -8,6 +8,7 @@ import keras
 from keras.models import load_model
 
 import base64
+import cv2
 from PIL import Image
 from io import BytesIO
 
@@ -17,10 +18,62 @@ cors = CORS(app, allow_headers='*',
             origins='*', methods='*', expose_headers='Authorization')
 
 
+def mask(img):
+    BLUR = 21
+    CANNY_THRESH_1 = 10
+    CANNY_THRESH_2 = 200
+    MASK_DILATE_ITER = 10
+    MASK_ERODE_ITER = 10
+
+    # BGR format
+    MASK_COLOR = (0.0, 0.0, 0.0)
+
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
+    edges = cv2.dilate(edges, None)
+    edges = cv2.erode(edges, None)
+
+    contour_info = []
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    for c in contours:
+        contour_info.append((
+            c,
+            cv2.isContourConvex(c),
+            cv2.contourArea(c),
+        ))
+    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+    max_contour = contour_info[0]
+
+    mask = np.zeros(edges.shape)
+    cv2.fillConvexPoly(mask, max_contour[0], (255))
+
+    mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+    mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+    mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+    mask_stack = np.dstack([mask]*3)
+
+    mask_stack = mask_stack.astype(
+        'float32') / 255.0
+    img = img.astype('float32') / 255.0
+
+    masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR)
+    masked = (masked * 255).astype('uint8')
+    masked = np.asarray(masked)
+
+    return masked
+
+
 def decode_image(data):
     decoded = base64.b64decode(data)
 
     img = Image.open(BytesIO(decoded))
+    img = mask(img)
+    img = Image.fromarray(img)
+    img = img.resize((28, 28))
+    img = img.convert('L')
     img = np.array(img)
     img = img.reshape(1, 28, 28, 1)
     img = img.astype('float32')
@@ -44,6 +97,7 @@ class WebServer(Resource):
         prediction = model.predict_classes(img)
 
         result = int(prediction[0])
+        print('Result:', result)
 
         return jsonify(result=result)
 
